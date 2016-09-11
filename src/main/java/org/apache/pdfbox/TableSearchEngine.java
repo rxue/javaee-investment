e515eb0b29c752cacbd5fbe084074766a119b36c
Rui Xue <rxue@ruixue.mobi>
2016-09-11 21:59:54 +0300

    TableSearchEngine is a method template of getting the table data from a pdf file

---
+++ src/main/java/org/apache/pdfbox/TableSearchEngine.java
@@ -0,0 +1,36 @@
+package org.apache.pdfbox;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.pdfbox.pdmodel.PDPage;
+
+public abstract class TableSearchEngine {
+	private String pdfSourceURLStr;
+	private byte[] pdfContentBytes;
+	
+	public void setContentBytes(String pdfSourceURLStr) 
+			throws IllegalArgumentException, IOException {
+		this.pdfSourceURLStr = pdfSourceURLStr;
+		URL pdfFilePath = new URL(this.pdfSourceURLStr);
+		this.pdfContentBytes = IOUtils.toByteArray(pdfFilePath);
+	}
+	
+	public abstract PDPage getPage(String tableKeyword);
+	
+	public abstract void getTable(PDPage page, String tableKeyword);
+	
+	public void getTable(String tableKeyword) {
+		 PDPage page = this.getPage(tableKeyword);
+		 getTable(page, tableKeyword);
+	}
+	
+	public void getTable(String pdfSourceURLStr, String tableKeyword) 
+			throws IllegalArgumentException, IOException {
+		if (! pdfSourceURLStr.equals(this.pdfSourceURLStr))
+			this.setContentBytes(pdfSourceURLStr);
+		this.getTable(tableKeyword);
+	}
+}
