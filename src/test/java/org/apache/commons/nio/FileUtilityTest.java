8b22672ba452ef6127166913db7735fbeaf345f4
dingding <ruixue.fi@gmail.com>
2018-07-31 00:36:46 +0300

    test FileAlreadyExistsException added

--- src/test/java/org/apache/commons/nio/FileUtilityTest.java
+++ src/test/java/org/apache/commons/nio/FileUtilityTest.java
@@ -1,15 +1,15 @@
 package org.apache.commons.nio;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.apache.commons.nio.FileUtility;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FileUtilityTest {
 
@@ -20,6 +20,20 @@ public class FileUtilityTest {
 			URL sourceURL = new URL("https://www.eon.com/content/dam/eon/eon-com/investors/annual-report/EON_Annual_Report_2015.pdf");
 			assertNotNull(FileUtility.copy(sourceURL, target));
 			Files.delete(target);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@Test
+	public void testCopyFromUrlWithoutReplaceExisting() {
+		Path target = Paths.get("existing.pdf");
+		try {
+			Files.createFile(target);
+			URL sourceURL = new URL("https://www.eon.com/content/dam/eon/eon-com/investors/annual-report/EON_Annual_Report_2015.pdf");
+			assertThrows(FileAlreadyExistsException.class, () -> FileUtility.copy(sourceURL, target));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
