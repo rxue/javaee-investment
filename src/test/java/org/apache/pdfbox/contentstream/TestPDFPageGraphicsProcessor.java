73bf58638d0c0c1c94db1929061d6a6ea804adc5
dingding <ruixue.fi@gmail.com>
2018-07-31 00:30:19 +0300

    tests commented

--- src/test/java/org/apache/pdfbox/contentstream/TestPDFPageGraphicsProcessor.java
+++ src/test/java/org/apache/pdfbox/contentstream/TestPDFPageGraphicsProcessor.java
@@ -16,33 +16,33 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TestPDFPageGraphicsProcessor {
-	private static PDDocument pdDocument;
-	@BeforeClass
-	public static void setUpOnce() throws IllegalArgumentException, IOException {
-		URL url = new URL("http://www.eon.com/content/dam/eon-com/ueber-uns/publications/EON_Annual_Report_2015_EN.pdf");
-		byte[] contents = IOUtils.toByteArray(url);
-		pdDocument = PDDocument.load(contents);
-	}
-	
-	@Test
-	public void testGetPageGraphics() {
-		PDPageTree pageTree = this.pdDocument.getPages();
-		Iterator<PDPage> pages = pageTree.iterator();
-		pages.next();
-		PDPage page = pages.next();
-		PDRectangle cropBox = page.getCropBox();
-		PDFPageGraphicsProcessor p = new PDFPageGraphicsProcessor(page);
-		try {
-			p.processPage();
-			p.transformGeneralPathsToTextQuadrant();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		List<GeneralPath> generalPaths = p.getGeneralPathList();
-		GraphicsDebugger debugger = new GraphicsDebugger();
-		debugger.showGraphics(Math.round(cropBox.getWidth()), Math.round(cropBox.getHeight()), generalPaths);
-		
-	}
+//	private static PDDocument pdDocument;
+//	@BeforeClass
+//	public static void setUpOnce() throws IllegalArgumentException, IOException {
+//		URL url = new URL("http://www.eon.com/content/dam/eon-com/ueber-uns/publications/EON_Annual_Report_2015_EN.pdf");
+//		byte[] contents = IOUtils.toByteArray(url);
+//		pdDocument = PDDocument.load(contents);
+//	}
+//	
+//	@Test
+//	public void testGetPageGraphics() {
+//		PDPageTree pageTree = this.pdDocument.getPages();
+//		Iterator<PDPage> pages = pageTree.iterator();
+//		pages.next();
+//		PDPage page = pages.next();
+//		PDRectangle cropBox = page.getCropBox();
+//		PDFPageGraphicsProcessor p = new PDFPageGraphicsProcessor(page);
+//		try {
+//			p.processPage();
+//			p.transformGeneralPathsToTextQuadrant();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		List<GeneralPath> generalPaths = p.getGeneralPathList();
+//		GraphicsDebugger debugger = new GraphicsDebugger();
+//		debugger.showGraphics(Math.round(cropBox.getWidth()), Math.round(cropBox.getHeight()), generalPaths);
+//		
+//	}
 
 }
